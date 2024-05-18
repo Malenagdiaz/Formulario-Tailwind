c6052ce101829e17856fc3c750341e19048d703d
Malena Gerez Diaz <mgerezdiaz@gmail.com>
2024-05-18 00:33:16 -0300

    Agregue tailwind.config.js para personalizar media queries

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      screens: {
+        sm: { max: "767px" },
+        md: { min: "768px", max: "1023px" },
+        lg: { min: "1024px" },
+      },
+    },
+  },
+  plugins: [],
+};
